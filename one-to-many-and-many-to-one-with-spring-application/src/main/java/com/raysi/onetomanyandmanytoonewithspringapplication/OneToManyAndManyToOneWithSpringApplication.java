4ae0130533483447959c68e3589f0a3a6609f403
Aashish Kumar Ray <65903203+Raysi2002@users.noreply.github.com>
2025-01-21 21:16:35 +0530

    One to Many

---
+++ one-to-many-and-many-to-one-with-spring-application/src/main/java/com/raysi/onetomanyandmanytoonewithspringapplication/OneToManyAndManyToOneWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.raysi.onetomanyandmanytoonewithspringapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToManyAndManyToOneWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneToManyAndManyToOneWithSpringApplication.class, args);
+    }
+
+}
---
+++ one-to-many-and-many-to-one-with-spring-application/src/test/java/com/raysi/onetomanyandmanytoonewithspringapplication/OneToManyAndManyToOneWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raysi.onetomanyandmanytoonewithspringapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToManyAndManyToOneWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
